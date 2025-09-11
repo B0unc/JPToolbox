3a509f8ac05a8752d57f3ed30c21d3c7d40ab48f
B0unc <Thomascollins212@gmail.com>
2025-09-11 12:04:25 -0700

    Fixed a bug for the debugger. The issue is with the SystemProperty

--- src/main/java/AppMainToolbox/AppMain.java
+++ src/main/java/AppMainToolbox/AppMain.java
@@ -11,6 +11,7 @@ public class AppMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("/Scenes/index.fxml"));
+        System.setProperty("javafx.sg.warn", "true"); // Should fix the crash when using a debugger (This is due to java trying tofind SystemProperty but does not check to see if its there before it does the find)
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("JPLToolbox");
         stage.setScene(scene);
